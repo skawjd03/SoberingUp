1576d36b537911be406a36682278f5a1df871093
skawjd03 <skawjd03@github.com>
2020-11-23 21:37:20 +0900

    new Project

---
+++ SpringBootNum1/src/main/java/com/sbSample/sb/SpringBootNum1Application.java
@@ -0,0 +1,13 @@
+package com.sbSample.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNum1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNum1Application.class, args);
+	}
+
+}
