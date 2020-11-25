d67605bb60aad48034e6fbe39195063d6ea0681e
skawjd03 <skawjd03@github.com>
2020-11-25 22:02:55 +0900

    commit

---
+++ SpringBootNum1-1/src/test/java/com/sbSample/sb/SpringBootNum11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbSample.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNum11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
