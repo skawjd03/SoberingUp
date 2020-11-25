4a6a8086880f54aada03a3d76eb113270dd1cfa6
skawjd03 <skawjd03@github.com>
2020-11-25 21:58:33 +0900

    commit

--- SpringBootNum1/src/main/java/com/sbSample/sb/SpringBootNum1Application.java
+++ SpringBootNum1/src/main/java/com/sbSample/sb/SpringBootNum1Application.java
@@ -2,12 +2,23 @@ package com.sbSample.sb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringBootNum1Application {
+	
+	public static String getStr(String str) throws Exception{	
+		return str;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootNum1Application.class, args);
+		try {
+			System.out.println(SpringBootNum1Application.getStr("hi"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
